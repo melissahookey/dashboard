44568fed2249c2d52916a23a8de13ca438ec243d
Melissa Hookey <melissahookey@Melissas-MBP-2.attlocal.net>
2023-12-20 21:05:27 -0600

    templates

---
+++ dashboard-app/src/components/Weather/Weather.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+export const Currentforecast = () => {
+    return (
+        <Card>
+            <Card.Body>
+                {/* Loction name fill in title */}
+                <Card.Title></Card.Title>
+                {/* Temp icon fill in img */}
+                <Card.Img></Card.Img>
+                {/* Temp fill in subtitle */}
+                <Card.Subtitle></Card.Subtitle>
+            </Card.Body>
+        </Card>
+    )
+}
